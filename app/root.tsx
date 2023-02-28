2f9da3599b49b86aee27581f3b9e2c497ba0927a
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2023-02-28 15:52:00 -0300

    chore: adicionando tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,8 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import styles from "./tailwind.css";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
