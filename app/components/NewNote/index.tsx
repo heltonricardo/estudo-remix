6091ce8d23119223fd8c11832532e4e4c357cc11
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2023-03-17 08:57:34 -0300

    feat: desabilitando botao ao submeter formulario

--- app/components/NewNote/index.tsx
+++ app/components/NewNote/index.tsx
@@ -1,7 +1,10 @@
-import { Form } from "@remix-run/react";
+import { Form, useTransition as useNavigation } from "@remix-run/react";
 import styles from "./styles.css";
 
 export default function NewNote() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
   return (
     <Form method="post" id="note-form">
       <p>
@@ -13,7 +16,7 @@ export default function NewNote() {
         <textarea id="content" name="content" rows={5} required />
       </p>
       <div className="form-actions">
-        <button>Add Note</button>
+        <button disabled={isSubmitting}>{isSubmitting ? "Adding..." : "Add Note"}</button>
       </div>
     </Form>
   );
