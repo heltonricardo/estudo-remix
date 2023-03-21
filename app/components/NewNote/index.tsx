d21d5aa23a427bd8343ceaf18b7ed1bf86194ad3
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2023-03-21 12:28:42 -0300

    feat: validando formulario com action data

--- app/components/NewNote/index.tsx
+++ app/components/NewNote/index.tsx
@@ -1,12 +1,14 @@
-import { Form, useTransition as useNavigation } from "@remix-run/react";
+import { Form, useActionData, useTransition as useNavigation } from "@remix-run/react";
 import styles from "./styles.css";
 
 export default function NewNote() {
+  const data = useActionData();
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
 
   return (
     <Form method="post" id="note-form">
+      {data?.message && <p>{data.message}</p>}
       <p>
         <label htmlFor="title">Title</label>
         <input type="text" id="title" name="title" required />
--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -7,6 +7,7 @@ import Note from "../models/Note";
 
 export default function Notes() {
   const notes = useLoaderData();
+
   return (
     <main>
       <NewNote />
@@ -21,10 +22,16 @@ export async function loader() {
 
 export async function action({ request }: ActionArgs) {
   const formData = await request.formData();
+
   // Use this to automatically get all the fields:
   // const note = Object.fromEntries(formData);
   // But we are using TS and the model, then:
   const note = new Note(formData);
+
+  if (note.title.trim().length < 5) {
+    return { message: "Invalid title: must be at least 5 characters long." };
+  }
+
   const existingNotes = await getStoredNotes();
   const updatedNotes = existingNotes.concat(note);
   await storeNotes(updatedNotes);
