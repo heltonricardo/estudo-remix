2e516ae9bbe9f74f11b4eb2b91b932a0f3191132
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2023-03-27 14:38:59 -0300

    feat: carregando dados para detalhes de nota

--- app/data/notes.ts
+++ app/data/notes.ts
@@ -3,13 +3,18 @@ import Note from "~/models/Note";
 
 const FILE_NAME = "notes.json";
 
-export async function getStoredNotes() {
+export function storeNotes(notes: Note[]) {
+  return fs.writeFile(FILE_NAME, JSON.stringify({ notes: notes || [] }));
+}
+
+export async function getStoredNotes(): Promise<Note[]> {
   const rawFileContent = await fs.readFile(FILE_NAME, { encoding: "utf-8" });
   const data = JSON.parse(rawFileContent);
   const storedNotes = data.notes ?? [];
   return storedNotes;
 }
 
-export function storeNotes(notes: Note[]) {
-  return fs.writeFile(FILE_NAME, JSON.stringify({ notes: notes || [] }));
+export async function getStoredNoteById(noteId: string) {
+  const notes = await getStoredNotes();
+  return notes.find((note) => note.id === noteId);
 }
--- app/routes/notes.$noteId.tsx
+++ app/routes/notes.$noteId.tsx
@@ -1,18 +1,28 @@
-import { Link } from "@remix-run/react";
+import { ActionArgs } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { getStoredNoteById } from "~/data/notes";
+import Note from "~/models/Note";
 import styles from "~/styles/note-details.css";
 
 export default function NoteDetailsPage() {
+  const note = useLoaderData<Note>();
+
   return (
     <main id="note-details">
       <header>
         <nav>
           <Link to="/notes">Back to all notes</Link>
         </nav>
-        <h1>Title</h1>
+        <h1>{note.title}</h1>
       </header>
-      <p id="note-details-content">Content</p>
+      <p id="note-details-content">{note.content}</p>
     </main>
   );
+}
+
+export async function loader({ params }: ActionArgs) {
+  const noteId = params.noteId || "";
+  return await getStoredNoteById(noteId);
 }
 
 export function links() {
