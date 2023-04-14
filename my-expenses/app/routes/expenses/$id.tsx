2a8cbd8dca8f1232c850de5808f32e1ea6a0fed1
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2023-04-14 18:11:19 -0300

    feat: fechando modal ao clicar no fundo

--- my-expenses/app/routes/expenses/$id.tsx
+++ my-expenses/app/routes/expenses/$id.tsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
 
 export default function UpdateExpensePage() {
+  const navigate = useNavigate();
+
+  function closeHandler() {
+    navigate("..");
+  }
+
   return (
-    <Modal onClose={() => {}}>
+    <Modal onClose={closeHandler}>
       <ExpenseForm />
     </Modal>
   );
--- my-expenses/app/routes/expenses/add.tsx
+++ my-expenses/app/routes/expenses/add.tsx
@@ -1,9 +1,15 @@
+import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
 
 export default function AddExpensesPage() {
+  const navigate = useNavigate();
+
+  function closeHandler() {
+    navigate("..");
+  }
   return (
-    <Modal onClose={() => {}}>
+    <Modal onClose={closeHandler}>
       <ExpenseForm />
     </Modal>
   );
